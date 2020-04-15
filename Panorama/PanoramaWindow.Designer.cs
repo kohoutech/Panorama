842588b327002c69eb20ba8642c5449b78b0172b
Kohoutech <george@kohoutech.com>
2020-04-15 13:05:10 -0400

    initial code commit - finally!

---
+++ Panorama/PanoramaWindow.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Panorama
+{
+    partial class PanoramaWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tbxAddress = new System.Windows.Forms.ToolStripTextBox();
+            this.lblContent = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbxAddress});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.BackColor = System.Drawing.Color.LightGray;
+            this.tbxAddress.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(400, 25);
+            this.tbxAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbxAddress_KeyDown);
+            // 
+            // lblContent
+            // 
+            this.lblContent.AutoSize = true;
+            this.lblContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblContent.Location = new System.Drawing.Point(0, 25);
+            this.lblContent.Name = "lblContent";
+            this.lblContent.Size = new System.Drawing.Size(0, 13);
+            this.lblContent.TabIndex = 3;
+            // 
+            // PanoramaWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblContent);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "PanoramaWindow";
+            this.Text = "Panorama";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox tbxAddress;
+        private System.Windows.Forms.Label lblContent;
+    }
+}
+
---
+++ Panorama/PanoramaWindow.cs
@@ -0,0 +1,55 @@
+﻿/* ----------------------------------------------------------------------------
+Panorama - a wide angled view of the web
+Copyright (C) 2011-2020  George E Greaney
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+----------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+//The Eiffel Tower and the Taj Mahal are mine to see on clear days
+//I can see for miles and miles and miles and miles and miles, oh yeah
+
+namespace Panorama
+{
+    public partial class PanoramaWindow : Form
+    {
+        public PanoramaWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                WebRequest request = WebRequest.Create(tbxAddress.Text);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string content = reader.ReadToEnd();
+                lblContent.Text = content;
+            }
+        }
+    }
+}
